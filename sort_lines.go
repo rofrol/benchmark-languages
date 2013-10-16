1f9e985eca7b653282b807caf6159da6711b619a
Roman Frołow <rofrol@gmail.com>
2013-10-16 15:00:00 +0100

    first commit

---
+++ sort_lines.go
@@ -0,0 +1,12 @@
+package main
+import "io/ioutil"
+import "sort"
+import "strings"
+import "os"
+func main() {
+	b, _ := ioutil.ReadFile(os.Args[1]) // Not interested in error handling now
+	lines := strings.Split(string(b), "\n")
+	lines = lines[:len(lines)-1] // Split adds empty line at the end
+	sort.Strings(lines)
+	_ = ioutil.WriteFile(os.Args[2], []byte(strings.Join(lines, "\n")), 0644)
+}
