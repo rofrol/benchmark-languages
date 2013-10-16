20c5b03740f1473595eef4ad0741427b6a88a6d9
Roman Frołow <rofrol@gmail.com>
2013-10-16 17:21:00 +0100

    go compiled first, python with -S should be faster

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 printf "\nsh"
 time sh sort_lines.sh $1 $2_sh.txt
 printf "\ngo"
-time go run sort_lines.go $1 $2_go.txt
+go build sort_lines.go
+time ./sort_lines $1 $2_go.txt
 printf "\npython"
-time python sort_lines.py $1 $2_py.otxt
+time python -S sort_lines.py $1 $2_py.otxt
 printf "\nruby"
 touch $2_rb.txt
 time ruby sort_lines.rb $1 $2_rb.txt
