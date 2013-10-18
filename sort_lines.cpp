f8235ef6e2f21d46180241ddd8888e4046a3950e
Roman Frołow <rofrol@gmail.com>
2013-10-18 16:00:59 +0100

    speed up c++, rm files if exist

--- sort_lines.cpp
+++ sort_lines.cpp
@@ -6,16 +6,19 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-  ifstream ifile(argv[1]);
-  ofstream ofile(argv[2]);
-  string line;
-  vector<string> data;
-  while(getline(ifile, line)) {
-    data.push_back(line);
-  }
-  sort(data.begin(), data.end());
-  for(const auto &i : data) {
-    ofile << i << std::endl;
-  }
-  return 0;
+	if (setvbuf(stdout, 0, _IOLBF, 4096) != 0) { // this enables buffering of stdout greatly increasing cout operation
+		abort();
+	}
+	ifstream ifile(argv[1]);
+	ofstream ofile(argv[2]);
+	string line;
+	vector<string> data;
+	while(getline(ifile, line)) {
+		data.push_back(line);
+	}
+	sort(data.begin(), data.end());
+	for(const auto &i : data) {
+		ofile << i << std::endl;
+	}
+	return 0;
 }
--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ sudo chrt -f 99 /usr/bin/time -f "\n***\ntime: %E\ncontext switches: %c\nwaits: 
 printf "\ngo"
 go build sort_lines.go
 sudo chrt -f 99 /usr/bin/time -f "\n***\ntime: %E\ncontext switches: %c\nwaits: %w" ./sort_lines $1 $2_go.txt
-rm sort_lines
+[ -f "sort_lines" ] && rm sort_lines
 printf "\npython"
 sudo chrt -f 99 /usr/bin/time -f "\n***\ntime: %E\ncontext switches: %c\nwaits: %w" python -S sort_lines.py $1 $2_py.otxt
 printf "\nruby"
@@ -20,7 +20,7 @@ sudo chrt -f 99 /usr/bin/time -f "\n***\ntime: %E\ncontext switches: %c\nwaits: 
 printf "\nc++"
 g++ -std=c++11 sort_lines.cpp
 sudo chrt -f 99 /usr/bin/time -f "\n***\ntime: %E\ncontext switches: %c\nwaits: %w" ./a.out $1 $2_go.txt
-rm a.out
+[ -f "a.out" ] && rm a.out
 
 echo
 echo "For more go to https://news.ycombinator.com/item?id=6559011"
